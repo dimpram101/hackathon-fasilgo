82b2db05d22b277dde2f37374c2bd7565b0b0370
Djibran1103 <11211070@student.itk.ac.id>
2023-03-18 12:28:53 +0800

    Sidebar & Dashboard Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './Page/Dashboard'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Dashboard from "./Page/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/dashboard/*" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Page/Dashboard.jsx
+++ frontend/src/Page/Dashboard.jsx
@@ -1,9 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom";
 import Sidebar from "../Component/Sidebar";
 
 const Dashboard = () => {
   return (
-    <Sidebar/>
-  )
-}
- 
+    <div className="flex flex-row">
+      <Sidebar />
+      <div className="flex flex-col mx-auto w-full p-2">
+        <Routes>
+            <Route path="/manajemen-fasilitas" element={<h2>Halo</h2>}/>
+            <Route path="/manajemen-transaksi" element={<h2>HEYOW</h2>}/>
+            <Route path="/manajemen-akun" element={<h2>HEYOW</h2>}/>
+            <Route path="/pengelolaan-fasilitas" element={<h2>HEYOW</h2>}/>
+            <Route path="/riwayat-transaksi" element={<h2>HEYOW</h2>}/>
+            <Route path="/fasilitas" element={<h2>HEYOW</h2>}/>
+            <Route path="/edit-akun" element={<h2>HEYOW</h2>}/>
+            {/* <Route path="/logout"> */}
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
 export default Dashboard;
