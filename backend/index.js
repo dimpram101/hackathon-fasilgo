4f6c92cb743d00bf5e0e4409e97d83fd6687ff73
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2023-03-18 10:46:39 +0800

    migrate database models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import { db } from "./models/Association.js";
 
 const app = express()
 
+try {
+  await db.sync({ force: true });
+  console.log("db connected")
+} catch (error) {
+  console.log(error)
+}
 
 app.get('/', (req, res) => {
   res.send("Test")
