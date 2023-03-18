766f0e8e5db49c2ec2507661bb4a50bdc784d62d
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2023-03-18 11:18:34 +0800

    Add controllers file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <h2></h2>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
