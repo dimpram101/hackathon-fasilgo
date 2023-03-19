264843db973f28a48f1ffdc5bbcf885a09707c9c
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2023-03-19 09:46:35 +0800

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  }
+  plugins: [react()]
 })
